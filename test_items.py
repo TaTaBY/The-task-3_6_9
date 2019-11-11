440d6dc3bb6f42a8ba9818034d08df1bc5e9af18
TaTaBY <work-home-light@mail.ru>
2019-11-12 01:29:54 +0300

    2 file ready

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+def test_access_to_button_for_adding_item_to_bucket(browser, language):
+
+    browser.get(language)
+    browser.implicitly_wait(10)
+    button1 = browser.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button")
+    if language == "http://selenium1py.pythonanywhere.com/fr/catalogue/coders-at-work_207":
+    	assert button1.text == "Ajouter au panier", "Sub-test failed"
+    time.sleep(30)
+    
