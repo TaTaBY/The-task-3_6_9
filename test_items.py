7d2302717654d2bed6afe76b98b0a8d9b1b6f566
TaTaBY <work-home-light@mail.ru>
2019-11-12 09:44:20 +0300

    final version for 2 file

--- test_items.py
+++ test_items.py
@@ -2,10 +2,10 @@ import time
 
 def test_access_to_button_for_adding_item_to_bucket(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207"
-    browser.get(language)
+    browser.get(link)
     browser.implicitly_wait(10)
     button1 = browser.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button")
     if language == "http://selenium1py.pythonanywhere.com/fr/catalogue/coders-at-work_207":
     	assert button1.text == "Ajouter au panier", "Sub-test failed"
-    time.sleep(30)
+    time.sleep(3)
     
