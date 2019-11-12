c0aee46b390663a4858645782e1542e76de86f25
TaTaBY <work-home-light@mail.ru>
2019-11-12 09:22:31 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
 def test_access_to_button_for_adding_item_to_bucket(browser, language):
-
+    link = 
     browser.get(language)
     browser.implicitly_wait(10)
     button1 = browser.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/form/button")
